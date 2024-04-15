7e0ff7ae37be2a33470507fef20d211607c4b889
stavros-vl <svlachos25@gmail.com>
2024-04-15 10:48:44 +0300

    add terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "sp500-data-tracker"
+  region  = "europe-west"
+}
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "sp500-tracker-terrabucket"
+  location      = "EU"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "sp500_tables"
+  project    = "sp500-data-tracker"
+  location   = "EU"
+}
