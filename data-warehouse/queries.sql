5bf7895c20d1aefc17c23847c21fd928e1a0c5d7
stavros-vl <svlachos25@gmail.com>
2024-06-30 14:54:48 +0000

    minor fix on queries.sql

--- data-warehouse/queries.sql
+++ data-warehouse/queries.sql
@@ -9,14 +9,14 @@ OPTIONS (
 CREATE OR REPLACE EXTERNAL TABLE `sp500-data-tracker.sp500_tables.sp500_wiki_data_v1`
 OPTIONS (
   format = "CSV",
-  uris = ['gs://sp500-tracker-terrabucket/sp500_wiki_data_20240420.csv']
+  uris = ['gs://sp500-tracker-terrabucket/sp500_wiki_data_20240420.csv.gz']
 );
 
 -- Create standard table for wiki data from CSV in GCS
 LOAD DATA OVERWRITE `sp500-data-tracker.sp500_tables.sp500_wiki_data_v2`
 FROM FILES (
   format = 'CSV',
-  uris = ['gs://sp500-tracker-terrabucket/sp500_wiki_data_20240420.csv']
+  uris = ['gs://sp500-tracker-terrabucket/sp500_wiki_data_20240420.csv.gz']
 );
 
 
